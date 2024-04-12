3ba6c5a803b347ff2c0a970c9b4821b84b5b33ea
charans29 <smodugula@csuchico.edu>
2024-04-11 18:11:26 -0700

    setting designs

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions } from 'react-native';
+
+const window = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/adaptive-icon.png')} style={styles.backgroundImage}/>
-      <Image source={require('./assets/favicon.png')} />
-      <Text style={{ color: 'red', textAlign:"center"}}>Designed App.js to start working on Dapp!</Text>
-      <StatusBar style="auto" />       
+      <ImageBackground source={require('./assets/adaptive-icon.png')}  style={styles.backgroundImage}/>
+      <View style={{position:'absolute', alignItems: 'center', top:window.height*0.1, left:window.width*0.3}}>
+        <Image source={require('./assets/favicon.png')} />
+        <Text style={{ color: 'red', textAlign: "center" }}>Designed App.js to start working on Dapp!</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -17,12 +21,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    justifyContent: 'flex-start',
   },
   backgroundImage: {
-    flex: 1,
-    width: '50%', 
-    height: '100%', 
-    marginRight:'auto'
+    width: '50%',
+    height: '100%',
+    marginRight: 'auto',
   },
 });
